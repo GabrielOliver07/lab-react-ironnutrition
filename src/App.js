8f47758ad6742904f02f347d9b440b41bc4d1110
GabrielOliver07 <107689225+GabrielOliver07@users.noreply.github.com>
2022-10-15 17:58:22 -0300

    interação 1,2 e 3 ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+import foodsJson from './foods.json';
+import { useState } from 'react';
+import FoodBox from './componentes/FoodBox';
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Food List</h1>
+      <div>
+        {foods.map(function (food) {
+          return (
+            <FoodBox food={food} />
+          )
+        })};
+      </div>
     </div>
   );
 }
